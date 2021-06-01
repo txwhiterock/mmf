[fix] fix TPU checkpoint saving, parameter broadcasting, and misc stuff
Fix the following on TPU training:
- instead of using `xm.save`, add a custom `save_xla_ckpt` to avoid
crashing on lr_scheduler state dict; **note that if we later switch back
to xm.save, we need to set `global_master=True` in xm.save**
- only delete old checkpoint from master process when using XLA
- only open file in "wb" mode from the master process when using XLA.
(Otherwise, concurrent writing to the same file in `with
PathManager.open(ckpt_filepath, "wb")` from multiple processes causes
permission denied error).
- only save `config.yaml` from the global master process.
- skip batch size validation in XLA mode in evaluation loop (since data
loader already automatically drops the last batch in XLA mode)
- On TPUs, there isn't an equivalent to DistributedDaraParallel yet, so
adding a manual parameter broadcasting at the beginning of training
(`broadcast_xla_master_model_param`). Otherwise we will have different
parameters on each process since we are explicitly setting different
seeds in different processes in MMF.
- set `drop_last=True` in DistributedSampler for XLA to correctly drop
the last batch in XLA mode (the current codebase only set
`drop_last=True` in DataLoader, which is not enough).

Test plan: tested on GCP.

mmf/utils/checkpoint.py
@@ -10,10 +10,11 @@ import warnings
 import torch
 from mmf.common.registry import registry
 from mmf.utils.configuration import get_mmf_env, load_yaml
-from mmf.utils.distributed import is_master, is_xla, synchronize
+from mmf.utils.distributed import is_master, is_xla, open_if_master, synchronize
 from mmf.utils.download import download_pretrained_model
 from mmf.utils.file_io import PathManager
 from mmf.utils.general import get_current_device, updir
+from mmf.utils.xla import save_xla_ckpt
 from omegaconf import OmegaConf
 
 
@@ -160,7 +161,8 @@ class Checkpoint:
         if not PathManager.exists(self.models_foldername):
             PathManager.mkdirs(self.models_foldername)
 
-        self.save_config()
+        if is_master():
+            self.save_config()
 
         self.repo_path = updir(os.path.abspath(__file__), n=3)
         self.git_repo = None
@@ -480,7 +482,7 @@ class Checkpoint:
         }
 
     def save_func(self, *args):
-        return xm.save(*args) if is_xla() else torch.save(*args)
+        return save_xla_ckpt(*args) if is_xla() else torch.save(*args)
 
     def save(self, update, iteration=None, update_best=False):
         # Only save in main process
@@ -545,22 +547,23 @@ class Checkpoint:
             git_metadata_dict = self._get_vcs_fields()
             ckpt.update(git_metadata_dict)
 
-        with PathManager.open(ckpt_filepath, "wb") as f:
+        with open_if_master(ckpt_filepath, "wb") as f:
             self.save_func(ckpt, f)
 
         if update_best:
             logger.info("Saving best checkpoint")
-            with PathManager.open(best_ckpt_filepath, "wb") as f:
+            with open_if_master(best_ckpt_filepath, "wb") as f:
                 self.save_func(ckpt, f)
 
         # Save current always
 
         logger.info("Saving current checkpoint")
-        with PathManager.open(current_ckpt_filepath, "wb") as f:
+        with open_if_master(current_ckpt_filepath, "wb") as f:
             self.save_func(ckpt, f)
 
         # Remove old checkpoints if max_to_keep is set
-        if self.max_to_keep > 0:
+        # In XLA, only delete checkpoint files in main process
+        if self.max_to_keep > 0 and is_master():
             if len(self.saved_iterations) == self.max_to_keep:
                 self.remove(self.saved_iterations.pop(0))
             self.saved_iterations.append(update)
@@ -582,5 +585,5 @@ class Checkpoint:
 
     def finalize(self):
         if is_master() or is_xla():
-            with PathManager.open(self.pth_filepath, "wb") as f:
+            with open_if_master(self.pth_filepath, "wb") as f:
                 self.save_func(self.trainer.model.state_dict(), f)

mmf/utils/distributed.py
@@ -1,5 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # Inspired from maskrcnn_benchmark, fairseq
+import contextlib
 import logging
 import os
 import pickle
@@ -86,11 +87,11 @@ def broadcast_tensor(tensor, src=0):
     with torch.no_grad():
         if is_xla():
             tensor = xm.all_to_all(
-                tensor.repeat([world_size, 1]),
+                tensor.repeat([world_size] + [1] * tensor.dim()),
                 split_dimension=0,
                 concat_dimension=0,
                 split_count=world_size,
-            )[0]
+            )[src]
         else:
             dist.broadcast(tensor, src=0)
 
@@ -128,13 +129,12 @@ def gather_tensor(tensor):
     with torch.no_grad():
         tensor_list = []
 
-        for _ in range(world_size):
-            tensor_list.append(torch.zeros_like(tensor))
-
         if is_xla():
             tensor_list = xm.all_gather(tensor)
             tensor_list = tensor_list.view(world_size, *tensor.size())
         else:
+            for _ in range(world_size):
+                tensor_list.append(torch.zeros_like(tensor))
             dist.all_gather(tensor_list, tensor)
             tensor_list = torch.stack(tensor_list, dim=0)
     return tensor_list
@@ -345,3 +345,12 @@ def suppress_output(is_master):
     # Log warnings only once
     warnings.warn = warn
     warnings.simplefilter("once", UserWarning)
+
+
+def open_if_master(path, mode):
+    from mmf.utils.file_io import PathManager
+
+    if is_master():
+        return PathManager.open(path, mode)
+    else:
+        return contextlib.nullcontext()